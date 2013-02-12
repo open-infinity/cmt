03bdab2c57cbd28bb1fe13cd0bf9464bf5032601
Pasi Kilponen <pasi.kilponen@tieto.com>
2013-02-12 10:33:35 +0200

    Default inserts to acl_cluster_type.

--- cloud/cloud-administrator/sql/cmt_extension_data.sql
+++ cloud/cloud-administrator/sql/cmt_extension_data.sql
@@ -6,3 +6,5 @@ insert into availability_zone_tbl (id, cloud_id, name) value(1, 0, "aws-cluster0
 insert into availability_zone_tbl (id, cloud_id, name) value(2, 0, "aws-cluster01");
 insert into availability_zone_tbl (id, cloud_id, name) value(3, 1, "dev-cluster01");
 
+insert into acl_cluster_type_tbl(`org_name`,`cluster_id`) select 'Cloud Admin', id from cluster_type_tbl;
+
