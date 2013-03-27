5d2efe5f7f05b324c18f4a96b6902628dae139ab
Ossi Hämäläinen <ossih@iki.fi>
2013-03-27 15:07:03 +0200

    Added worker start/stop scripts

---
+++ cloud/cloud-worker/scripts/stopworker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export JAVA_HOME=/etc/alternatives/java_sdk_openjdk/
+/usr/bin/jsvc -user toas -home /etc/alternatives/java_sdk_openjdk/ -pidfile /opt/toas/cloudmanagementtools/worker/worker.pid -stop -cp /opt/toas/cloudmanagementtools/worker/cloud-worker-2.0.0.jar org.openinfinity.cloud.application.worker.CloudWorker
