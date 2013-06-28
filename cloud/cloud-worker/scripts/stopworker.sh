862b2d19c88f2455ab53260fd6344969a2b7b368
Tommi Siitonen <tommi.j.siitonen@gmail.com>
2013-06-28 12:15:29 +0300

    version change

--- cloud/cloud-worker/scripts/stopworker.sh
+++ cloud/cloud-worker/scripts/stopworker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JAVA_HOME=/etc/alternatives/java_sdk_openjdk/
-/usr/bin/jsvc -user toas -home /etc/alternatives/java_sdk_openjdk/ -pidfile /opt/toas/cloudmanagementtools/worker/worker.pid -stop -cp /opt/toas/cloudmanagementtools/worker/cloud-worker-2.0.0.jar org.openinfinity.cloud.application.worker.CloudWorker
+/usr/bin/jsvc -user toas -home /etc/alternatives/java_sdk_openjdk/ -pidfile /opt/toas/cloudmanagementtools/worker/worker.pid -stop -cp /opt/toas/cloudmanagementtools/worker/cloud-worker-1.2.0.RELEASE.jar org.openinfinity.cloud.application.worker.CloudWorker
