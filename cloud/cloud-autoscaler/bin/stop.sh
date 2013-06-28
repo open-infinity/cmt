d6e8f5ae6311260c1c647c182c348a29cdb281cf
Vedran Bartonicek <vbartoni@gmail.com>
2013-06-28 12:21:03 +0300

    Fix for Autoscaler start and stop scripts-version update.

--- cloud/cloud-autoscaler/bin/stop.sh
+++ cloud/cloud-autoscaler/bin/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CP=/opt/toas/cloudmanagementtools/autoscaler/lib/java/jar/cloud-autoscaler-1.0.0.jar
+CP=/opt/toas/cloudmanagementtools/autoscaler/lib/java/cloud-autoscaler-1.2.0.RELEASE.jar
 PID_FILE=/opt/toas/cloudmanagementtools/autoscaler/var/run/autoscaler.pid
 
 /usr/bin/jsvc -stop -pidfile $PID_FILE  -cp $CP org.openinfinity.cloud.autoscaler.application.Autoscaler
