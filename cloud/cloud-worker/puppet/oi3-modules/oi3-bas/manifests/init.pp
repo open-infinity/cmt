0c97ce096e31f7f9172d1c22e92e4f9f444ee4d6
Tommi Siitonen <tommi.j.siitonen@gmail.com>
2013-09-23 11:00:51 +0300

    requirements fixed for oi3

--- cloud/cloud-worker/puppet/oi3-modules/oi3-bas/manifests/init.pp
+++ cloud/cloud-worker/puppet/oi3-modules/oi3-bas/manifests/init.pp
@@ -1,4 +1,5 @@
 class oi3-bas {
+	require oi3-basic	
 	include oi3-bas::install
 	include oi3-bas::config
 	include oi3-bas::service
--- cloud/cloud-worker/puppet/oi3-modules/oi3-bas/manifests/install.pp
+++ cloud/cloud-worker/puppet/oi3-modules/oi3-bas/manifests/install.pp
@@ -1,5 +1,5 @@
 class oi3-bas::install {
-	package { ["oi3-bas"]:
+	package { ["java-1.6.0-openjdk","oi3-bas"]:
 		ensure => present,
 		require => Class["oi3-basic"]
 	}
