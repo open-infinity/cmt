8023dc1639de827b7256ae03633def2c14474041
Vedran Bartonicek <vbartoni@gmail.com>
2013-10-03 16:20:46 +0300

    Updated worker start/stop scripts with Timo's changes on server.

--- cloud/cloud-worker/scripts/stopworker.sh
+++ cloud/cloud-worker/scripts/stopworker.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
 export JAVA_HOME=/etc/alternatives/java_sdk_openjdk/
-/usr/bin/jsvc -user toas -home /etc/alternatives/java_sdk_openjdk/ -pidfile /opt/toas/cloudmanagementtools/worker/worker.pid -stop -cp /opt/toas/cloudmanagementtools/worker/cloud-worker-1.2.0.RELEASE.jar org.openinfinity.cloud.application.worker.CloudWorker
+
+CLASS_PATH=/opt/toas/cloudmanagementtools/worker/cloud-worker-1.2.2.jar
+PID_FILE=/opt/toas/cloudmanagementtools/worker/worker.pid
+LOG_DIR=/opt/toas/cloudmanagementtools/worker/logs
+MAIN_CLASS=org.openinfinity.cloud.application.worker.CloudWorker
+
+/usr/bin/jsvc -user toas -home $JAVA_HOME -pidfile $PID_FILE -outfile $LOG_DIR/stdout.log -errfile $LOG_DIR/stderr.log -stop -cp  $CLASS_PATH $MAIN_CLASS
+
+
