61b0514495d054642bc053f18b9e2c2770ae7418
Tomi Lapinlampi <tomi.lapinlampi@tieto.com>
2013-10-04 13:06:20 +0300

    declare MULE_HOME in setenv.sh

--- cloud/cloud-worker/puppet/oi3-modules/oi3-serviceplatform/files/setenv.sh
+++ cloud/cloud-worker/puppet/oi3-modules/oi3-serviceplatform/files/setenv.sh
@@ -1,1 +1,2 @@
 JAVA_OPTS="-Dltt.configuration.filename=/opt/openinfinity/2.0.0/ltt-watch/ltt.xml -Dwebconsole.type=properties -Dwebconsole.jms.url=tcp://localhost:61616 -Dwebconsole.jmx.url=service:jmx:rmi:///jndi/rmi://localhost:1099/jmxrmi -Dwebconsole.jmx.user= -Dwebconsole.jmx.password="
+MULE_HOME="/opt/openinfinity/3.0.0/tomcat/lib/oi-mule-libs"
