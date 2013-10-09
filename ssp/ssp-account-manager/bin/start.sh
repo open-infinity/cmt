04323a16a5035b3f99e85d1ebe4a161eb7d97fd4
Vedran Bartonicek <vbartoni@gmail.com>
2013-10-09 17:09:47 +0300

    Created skeleton for ssp-account-manager: a Spring batch component for processing SSP accounts (charging, user authorization etc)

---
+++ ssp/ssp-account-manager/bin/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+USER=toas
+CP=/opt/toas/cloudmanagementtools/autoscaler/lib/java/cloud-autoscaler-1.2.0.RELEASE.jar
+PID_FILE=/opt/toas/cloudmanagementtools/autoscaler/var/run/autoscaler.pid
+JAVA_HOME=/etc/alternatives/java_sdk_openjdk/
+
+/usr/bin/jsvc -user $USER -home $JAVA_HOME -pidfile $PID_FILE  -cp $CP org.openinfinity.cloud.autoscaler.application.Autoscaler
