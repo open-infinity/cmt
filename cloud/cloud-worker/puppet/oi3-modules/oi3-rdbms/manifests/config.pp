ec8a6b3549dc6e94508b0ca5123f795826864d4a
Tommi Siitonen <tommi.j.siitonen@gmail.com>
2014-03-10 09:56:52 +0200

    MariaDB default init script and configurations taken into use. Old files removed.

--- cloud/cloud-worker/puppet/oi3-modules/oi3-rdbms/manifests/config.pp
+++ cloud/cloud-worker/puppet/oi3-modules/oi3-rdbms/manifests/config.pp
@@ -1,11 +1,11 @@
 class oi3-rdbms::config {
-	file {"/etc/my.cnf":
-		ensure => present,
-		source => "puppet:///modules/oi3-rdbms/my.cnf_client",
-		owner => "root",
-		group => "root",
-		mode => 0755,
-		require => Class["oi3-rdbms::install"],
+    file {"/etc/my.cnf.d/openinfinity.cnf":
+               ensure => present,
+               source => "puppet:///modules/oi3-rdbms/openinfinity.cnf",
+               owner => "root",
+               group => "root",
+               mode => 0755,
+               require => Class["oi3-rdbms::install"],
 	}
 
 	file {"/opt/openinfinity/3.0.0/rdbms/log":
@@ -17,8 +17,8 @@ class oi3-rdbms::config {
 	}
 
 	exec {"create-mysql-database":
-                unless => "/usr/bin/test -f /opt/openinfinity/3.0.0/rdbms/data/mysql/user.frm",
-                command => "/usr/bin/mysql_install_db --user=mysql --defaults-file=/opt/openinfinity/3.0.0/rdbms/data/my.cnf",
-                require => Class["oi3-rdbms::install"],
+               unless => "/usr/bin/test -f /opt/openinfinity/3.0.0/rdbms/data/mysql/user.frm",
+               command => "/usr/bin/mysql_install_db --user=mysql --defaults-file=/etc/my.cnf", 
+               require => Class["oi3-rdbms::install"],
         }
 }	
--- cloud/cloud-worker/puppet/oi3-modules/oi3-rdbms/manifests/install.pp
+++ cloud/cloud-worker/puppet/oi3-modules/oi3-rdbms/manifests/install.pp
@@ -24,29 +24,11 @@ class oi3-rdbms::install {
                 require => file["/opt/openinfinity/3.0.0/rdbms"],
         }	
 
-
-	file {"/opt/openinfinity/3.0.0/rdbms/data/my.cnf":
+	package { "oi3-rdbms":
 		ensure => present,
-		source => "puppet:///modules/oi3-rdbms/my.cnf",
-		owner => "mysql",
-		group => "mysql",
-		mode => 0644,
 		require => file["/opt/openinfinity/3.0.0/rdbms/data"],
 	}
 
-	package { "oi3-rdbms-3.0.0-1":
-		ensure => present,
-		require => file["/opt/openinfinity/3.0.0/rdbms/data/my.cnf"],
-	}
-
-	file { "/etc/init.d/mysql":
-		ensure => present,
-		source => "puppet:///modules/oi3-rdbms/mysql",
-		owner => "root",
-		group => "root",
-		mode => 0755,
-		require => package["oi3-rdbms-3.0.0-1"],
-	}
 
 	user { "mysql":
 		ensure => present,
