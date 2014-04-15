82cfec7e41d35d835a5cefc84e29b0abeae29841
Tommi Siitonen <tommi.j.siitonen@gmail.com>
2014-04-15 16:27:49 +0300

    Separate dbs for every sp node.

--- cloud/cloud-worker/puppet/oi3-modules/oi3-activemq-rdbms/manifests/service.pp
+++ cloud/cloud-worker/puppet/oi3-modules/oi3-activemq-rdbms/manifests/service.pp
@@ -1,10 +1,14 @@
 class oi3-activemq-rdbms::service {
 
-	exec { "oi3-create-activemq-db":
-		unless => "/usr/bin/mysql -uroot -p${mysql_password} toasactivemq",
-		command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"create database toasactivemq; grant all privileges on toasactivemq.* to
-			'activemq'@'%' identified by '${amq_password}'; flush privileges;\"",
-		require => Class["oi3-rdbms::service"],
-	}
-	
+        $nodeids = parsejson($basmachineidlist)
+
+        define amqdbcreate {
+                exec { "oi3-create-activemq-db-${name}":
+                        unless => "/usr/bin/mysql -uroot -p${mysql_password} toasamq${name}",
+                        command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"create database toasamq${name}; grant all privileges on toasamq${name}.* to 'activemq'@'%' identified by '${amq_password}'; flush privileges;\"",
+                        require => Class["oi3-rdbms::service"],
+               }
+        }
+
+        amqdbcreate { $nodeids: }
 }
