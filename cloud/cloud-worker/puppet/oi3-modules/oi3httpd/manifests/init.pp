72cb492ebc51940575244913aad181ee5013de3a
Ossi Hämäläinen <ossih@iki.fi>
2014-05-14 14:22:02 +0300

    Added parameter to help install without shibboleth

--- cloud/cloud-worker/puppet/oi3-modules/oi3httpd/manifests/init.pp
+++ cloud/cloud-worker/puppet/oi3-modules/oi3httpd/manifests/init.pp
@@ -3,14 +3,16 @@ class oi3httpd {
                 ensure => installed
         }
 
-	file { "/etc/httpd/conf.d/shib.conf.toas":
-                source => "puppet:///modules/oi3httpd/shib.conf",
-                replace => true,
-                owner => "root",
-                group => "root",
-                mode => 0644,
-                require => Package["httpd"],
-        }
+	if $::spused {
+		file { "/etc/httpd/conf.d/shib.conf.toas":
+                	source => "puppet:///modules/oi3httpd/shib.conf",
+                	replace => true,
+                	owner => "root",
+                	group => "root",
+                	mode => 0644,
+                	require => Package["httpd"],
+        	}
+	}
 
         file { "/etc/httpd/conf.d/oi3-proxy.conf.toas":
                 source => "puppet:///modules/oi3httpd/oi3-proxy.conf",
