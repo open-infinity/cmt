3cdaf75be750ed1bcd24a44fdb606a0e3fa9811e
Timo Saarinen <timo.saarinen@tieto.com>
2014-06-26 14:37:28 +0300

    MongoDB mini Puppet module for single-node stand-alone MongoDB installations

--- cloud/cloud-worker/puppet/oi3-modules/oi3apacheds_dump/files/apacheds-dump.sh
+++ cloud/cloud-worker/puppet/oi3-modules/oi3apacheds_dump/files/apacheds-dump.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 # Dump directory
-BACKUP_DIR=/opt/openinfinity/3.1.0/backup/dumps
+BACKUP_DIR=/opt/openinfinity/3.1.0/backup/dumps/apacheds
 mkdir -p $BACKUP_DIR &>/dev/null
 
 # ApacheDS version specific directories
 APACHEDS=`cd /etc/init.d ; ls apacheds* | tail -1` || exit 1
-APACHEDS_DATADIR=var/lib/`ls /var/lib/ | grep apacheds | tail -1`/default || exit 1
+APACHEDS_DATADIR=var/lib/`ls -t /var/lib/ | grep apacheds | head -1`/default || exit 1
+
+# Clean old backups before taking a new one
+cd $BACKUP_DIR && find . -type f -mtime +7 -name "apacheds-default-backup_*.tgz" -delete
 
 # ApacheDS doesn't support hot backup, that leaves us with this situation
 service $APACHEDS stop || exit 1
-cd / && tar -czf $BACKUP_DIR/apacheds-default-backup.tgz $APACHEDS_DATADIR/*
+cd / && tar -czf $BACKUP_DIR/apacheds-default-backup_`date +"%Y-%m-%d"`.tgz $APACHEDS_DATADIR/*
 service $APACHEDS start
 
---
+++ cloud/cloud-worker/puppet/oi3-modules/oi3mongodbmini/manifests/init.pp
@@ -0,0 +1,20 @@
+
+# This is a simple plain stand-alone MongoDB installation running on the 
+# default port 27017.
+#
+# You have to run the following command as root in puppet master machine.
+#
+#   puppet module install puppetlabs-mongodb
+#
+
+class oi3mongodbmini {
+	require oi3-basic
+	include '::mongodb::client'
+	include '::mongodb::server'
+}
+
+class {'::mongodb::server':
+    smallfiles => true,
+    bind_ip => '0.0.0.0',
+}
+
